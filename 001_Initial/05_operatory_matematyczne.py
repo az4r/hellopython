a4a8ee07808b7af571d7ca36bad4359898ddf7b3
az4r <zarzycki.arek@gmail.com>
2022-09-17 20:12:20 +0200

    Create 05_operatory_matematyczne.py

---
+++ 001_Initial/05_operatory_matematyczne.py
@@ -0,0 +1,35 @@
+a = 5
+b = 2
+
+#Dodawanie
+print(a + b)
+print(type(a + b))
+
+#Odejmowanie
+print(a - b)
+print(type(a - b))
+
+#Mnożneie
+print(a * b)
+print(type(a * b))
+
+#Dzielenie (wynik powinien być w postaci liczby zmiennoprzecinkowej - float)
+print(a / b)
+print(type(a / b))
+
+#Reszta z dzielenia
+print(a % b)
+print(type(a % b))
+
+#Potęgowanie
+print(a ** b)
+print(type(a ** b))
+
+#Dodawanie wartości do zmiennej
+a = 6
+a = a + 1
+print(a)
+#lub
+a = 6
+a += 1
+print(a)
